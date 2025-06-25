966408036690e67806eb686a30db4ea6fe928501
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-06-24 21:50:04 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def pagina_chat():
+    st.header('😃 Bem vindo ao Pergunte-me!', divider=True)
+
+
+def main():
+    pagina_chat()
+
+
+if __name__ == "__main__":
+    main()
