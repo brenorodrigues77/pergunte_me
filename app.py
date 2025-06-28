f08e701532cfaa6c4511340fe41571fd7d6dbf40
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-06-27 22:22:49 -0300

    Implementando IA com langchain
    Nesse commit foi iniciado a inclusão de inteligência artificial usando o framework Langchain e a api da Groq, além de funcionalidade na sidebar para que o usuário possa escolher qual Api e modelo ele ira querer usar para gerar respostas ao programa "Pergunte-me".

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
 import streamlit as st
+from langchain.memory import ConversationBufferMemory
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 
 tipo_de_arquivo = ['site', 'youtube', 'pdf', 'csv', 'txt']
 
-mensagens_exemplo = [
-    ('user', 'Oi'),
-    ('assistant', 'Olá, como posso ajudar?'),
-    ('user', 'tudo otimo')
-]
+modelos_ai = {
+    'OpenAI':  {'modelos': ['gpt-4', 'GPT-4.1 nano', 'o4-mini'], 'chat': ChatOpenAI},
+    'Groq': {'modelos': ['gemma2-9b-it', 'llama-3.3-70b-versatile', 'meta-llama/llama-guard-4-12b', 'qwen-qwq-32b'], 'chat': ChatGroq},
+}
+
+MEMORIA = ConversationBufferMemory()
 
 
-def pagina_chat():
+def load_modelo(modelos, modelos_selecionado, api_key):
+    chat = modelos_ai[modelos]['chat'](
+        model=modelos_selecionado,
+        api_key=api_key)
+
+    st.session_state['chat'] = chat
+
+
+def page_chat():
 
     st.set_page_config(page_title='Pergunte-me',
                        page_icon=':robot_face:', layout='wide')
 
     st.header('😃 Bem vindo ao Pergunte-me!', divider=True)
 
-    mensseger = st.session_state.get('mensseger', mensagens_exemplo)
+    mensseger = st.session_state.get('memoria', MEMORIA)
 
-    for message in mensseger:
-        chat = st.chat_message(message[0])
-        chat.markdown(message[1])
+    chat_model = st.session_state.get('chat')
+    for mensagem in mensseger.buffer_as_messages:
+        chat = st.chat_message(mensagem.type)
+        chat.markdown(mensagem.content)
 
     input_user = st.chat_input('Pergunte-me algo!')
 
     if input_user:
-        mensseger.append(('user', input_user))
-        st.session_state['mensseger'] = mensseger
-        st.rerun()
+        mensseger.chat_memory.add_user_message(input_user)
+        chat = st.chat_message('human')
+        chat.markdown(input_user)
+
+        chat = st.chat_message('ai')
+        response = chat.write_stream(chat_model.stream(input_user))
+        mensseger.chat_memory.add_ai_message(response)
+
+        st.session_state['memoria'] = mensseger
 
 
 def sidebar():
@@ -45,9 +64,23 @@ def sidebar():
         if tipos_de_arquivos == 'txt':
             file = st.file_uploader('suba seu txt', type=['.txt'])
 
+    with table[1]:
+        modelos = st.selectbox('selecione seus modelos', modelos_ai.keys())
+        modelos_selecionado = st.selectbox(
+            'selecione seus modelos', modelos_ai[modelos]['modelos'])
+        api_key = st.text_input(
+            f'copie e cole sua api key, {modelos}',
+            value=st.session_state.get(f'api_key {modelos}'),
+            type='password')
+
+        st.session_state[f'api_key {modelos}'] = api_key
+
+    if st.button('carregar Pergunta-me', use_container_width=True):
+        load_modelo(modelos, modelos_selecionado, api_key)
+
 
 def main():
-    pagina_chat()
+    page_chat()
     with st.sidebar:
         sidebar()
 
